0a95ea2ba329348a62d9f007447189e79b7a4560
Henri <couvreurhenri@gmail.com>
2018-11-24 16:05:36 +0100

    test scriptable object

---
+++ RescueSaber/Assets/Scriptable objects/Balancing.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCargoType", menuName = "NewCargoType", order = 1)]
+public class BusBalancing: ScriptableObject
+{
+    public Sprite sprite;
+    public Color color = Color.white;
+}
